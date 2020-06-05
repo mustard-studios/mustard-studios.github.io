d274c2b324f76a3add4ca643adf6ea913f92cf09
Jason Mustard <mustard.jason@gmail.com>
2020-06-04 17:25:57 -0700

    Created change bg_color button

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+function random_bg_color() {
+    var body = document.body;
+    var brand = body.getElementsByTagName('h1');
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    body.style.background = bgColor;    
+};
+
+
 function myFunction() {
     var element = document.body;
+    var writing = element.getElementsByTagName('i');
+    element.removeAttribute('style');
     element.classList.toggle('dark');
-}
+};
