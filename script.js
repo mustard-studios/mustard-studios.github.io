2afd1c82b72d01a71abdb4552b8d9e297701ce1d
Jason Mustard <mustard.jason@gmail.com>
2020-08-01 18:50:19 -0700

    Add draggable div

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function random_bg_color() {
     var body = document.body;
-    var card = document.getElementById('card');
+    var card = document.getElementById('mydiv');
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
@@ -21,7 +21,7 @@ function randomImage() {
 
 function darkMode() {
     var element = document.body;
-    var card = document.getElementById('card');
+    var card = document.getElementById('mydiv');
     element.removeAttribute('style');
     element.classList.toggle('dark');
     card.removeAttribute('style');
@@ -35,3 +35,71 @@ function mailTo() {
 function myResume() {
     window.open('https://github.com/mustard-studios/mustard-studios.github.io/blob/master/assets/documents/jMustardResume.pdf', '_blank');
 }
+
+
+// Drag card event
+var container = document.querySelector('#wrapper');
+var dragItem = document.querySelector('#mydiv');
+
+    var active = false;
+    var currentX;
+    var currentY;
+    var initialX;
+    var initialY;
+    var xOffset = 0;
+    var yOffset = 0;
+
+    container.addEventListener("touchstart", dragStart, false);
+    container.addEventListener("touchend", dragEnd, false);
+    container.addEventListener("touchmove", drag, false);
+
+    container.addEventListener("mousedown", dragStart, false);
+    container.addEventListener("mouseup", dragEnd, false);
+    container.addEventListener("mousemove", drag, false);
+
+    function dragStart(e) {
+      if (e.type === "touchstart") {
+        initialX = e.touches[0].clientX - xOffset;
+        initialY = e.touches[0].clientY - yOffset;
+      } else {
+        initialX = e.clientX - xOffset;
+        initialY = e.clientY - yOffset;
+      }
+
+      if (e.target === dragItem) {
+        active = true;
+      }
+    }
+
+    function dragEnd(e) {
+      initialX = currentX;
+      initialY = currentY;
+
+      active = false;
+    }
+
+    function drag(e) {
+      if (active) {
+      
+        e.preventDefault();
+      
+        if (e.type === "touchmove") {
+          currentX = e.touches[0].clientX - initialX;
+          currentY = e.touches[0].clientY - initialY;
+        } else {
+          currentX = e.clientX - initialX;
+          currentY = e.clientY - initialY;
+        }
+
+        xOffset = currentX;
+        yOffset = currentY;
+
+        setTranslate(currentX, currentY, dragItem);
+      }
+    }
+
+    function setTranslate(xPos, yPos, el) {
+      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    }
+
+
